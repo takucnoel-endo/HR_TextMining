7069fea308b7755cee7cc848176854915e8f6837
Taku Charles-Noel Endo <77642463+takucnoel-endo@users.noreply.github.com>
2022-03-18 14:39:51 -0500

    Add files via upload

---
+++ PreprocessingV1.R
@@ -0,0 +1,30 @@
+#Install required package if not found.
+if (!require("readxl")) install.packages("readxl")
+#Load required libraries
+library(readxl)#For reading excel sheet.
+library(tm)
+library(stringr)
+
+#Read data into R session.
+data <- read_excel('/Users/takucnoelendo/Documents/SP 2022/Consulting/HR Project/Data/Parse_data.xlsx')
+
+#Make a copy of the data.
+data <- data_raw
+
+#Rename summary columns name.
+#Use raw string.
+colnames(data)[which(names(data) ==
+                       r"{\T\TSUMMARY}")] <- "SUMMARY"
+
+#Remove unnesessary features from the dataset.
+subset_vec <- c('JOBFAMILY','NUMBEROFDIRECTREPORTS','NUMBEROFINDIRECTREPORTS')
+data <- data[, !(colnames(data) %in% subset_vec)]
+
+#For this data, the only features that needs to be kept separate from other columns are ...
+#(Position, Department, Jobcode, Paygrade, Prepared Date)
+data$Text <-  str_c(data$SUMMARY, '', data$JOBDUTIES)
+
+
+#Group columns by columns to be joined together, and columns that is not important for analysis.
+
+colnames(data)
